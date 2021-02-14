f12f60a54dbcc8bcb373866c6e87d90133c6cdc8
iwishiwasaneagle <jh.ewers@gmail.com>
2021-02-14 15:46:52 +0000

    Shorter delay

--- examples/blinky.cpp
+++ examples/blinky.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #define LED_PIN 15
+#define DELAY 100
 
 int main(void){
     wiringPiSetup();
@@ -9,14 +10,14 @@ int main(void){
     for(int i=0;i<10;i++){
         
         digitalWrite(LED_PIN,HIGH);
-        std::cout << "blink" << std::endl;
+        std::cout << "Blinky: bl1nk" << std::endl;
 
-        delay(1000);
+        delay(DELAY);
 
         digitalWrite(LED_PIN,LOW);
-        std::cout << "blonk" << std::endl;
+        std::cout << "Blinky: bl0nk" << std::endl;
 
-        delay(1000);
+        delay(DELAY);
     
     }
 
